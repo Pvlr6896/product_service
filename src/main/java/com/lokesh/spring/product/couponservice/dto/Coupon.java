217a5d5b45d2664c18ad0feb2b2184fb48059d17
lokesh <pathapatilokesh@gmail.com>
2023-07-23 12:09:25 +0530

    initail code

---
+++ src/main/java/com/lokesh/spring/product/couponservice/dto/Coupon.java
@@ -0,0 +1,34 @@
+package com.lokesh.spring.product.couponservice.dto;
+
+public class Coupon {
+	
+	private String code;
+	private Double discount;
+	private String expiryDate;
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public Double getDiscount() {
+		return discount;
+	}
+
+	public void setDiscount(Double discount) {
+		this.discount = discount;
+	}
+
+	public String getExpiryDate() {
+		return expiryDate;
+	}
+
+	public void setExpiryDate(String expiryDate) {
+		this.expiryDate = expiryDate;
+	}
+
+
+}
